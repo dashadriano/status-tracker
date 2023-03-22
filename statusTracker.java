472f814209c4b657b6a9b2f675f20da536103a56
Dash Adriano <86641365+dashadriano@users.noreply.github.com>
2023-03-22 08:23:18 +0700

    update statusTracker.java
    updated frame title: generalized to status tracker instead of COVID status tracker. removed spacing on imports section, made color calls lowercase

--- statusTracker.java
+++ statusTracker.java
@@ -4,7 +4,6 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,9 +11,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-public class COVIDtracker {
+public class statusTracker {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("COVID Status Tracker");
+        JFrame frame = new JFrame("Status Tracker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
@@ -31,7 +30,7 @@ public class COVIDtracker {
         for (int i = 0; i < 10; i++) {
             nameFields[i] = new JTextField(names[i]);
             status[i] = new JLabel("NULL", SwingConstants.CENTER);
-            status[i].setForeground(Color.GRAY);
+            status[i].setForeground(Color.gray);
             buttons[i] = new JButton("Toggle");
 
             int index = i;
@@ -40,14 +39,14 @@ public class COVIDtracker {
                 public void actionPerformed(ActionEvent e) {
                     if (status[index].getText().equals("NULL")) {
                         status[index].setText("Positive");
-                        status[index].setForeground(Color.RED);
+                        status[index].setForeground(Color.red);
                     }
                     if (status[index].getText().equals("Positive")) {
                         status[index].setText("Negative");
-                        status[index].setForeground(Color.GREEN);
+                        status[index].setForeground(Color.green);
                     } else {
                         status[index].setText("Positive");
-                        status[index].setForeground(Color.RED);
+                        status[index].setForeground(Color.red);
                     }
                 }
             });
